de71efcd0671d9558e7f4ca947f89044bd75abe5
Kay <hello@kunalpanchal.in>
2017-09-06 01:23:03 +0530

    Update git-backup.sh

--- git-backup.sh
+++ git-backup.sh
@@ -1,26 +1,33 @@
-declare -a repos=("py" "_" "code" "javascript-algorithms" "js" "node-api-generator" "kunalpanchal.github.io" "BasicsCpp" "java"
-"Outgrow-liveCalcs" "og-cheatbook"  "crx-whatsapp-share" "kunalpanchal-node" "football"
-"sitaby-android" "AmIRooted" "disguised-encrypter")
+#!/bin/bash
+
+# update the user/company and the repos that are to be backedup
+
 declare user_or_company="kunalpanchal"
+declare -a repos=("py" "_" "code" "javascript-algorithms" "js" "node-api-generator" "kunalpanchal.github.io" "BasicsCpp" "java"
+    "Outgrow-liveCalcs" "og-cheatbook"  "crx-whatsapp-share" "kunalpanchal-node" "football"
+"sitaby-android" "AmIRooted" "disguised-encrypter")
 
-# declare -a repos=("og-build" "Outgrow-frontend" "Outgrow-backend" "og-admin")
 # declare user_or_company="venturepact"
+# declare -a repos=("og-build" "Outgrow-frontend" "Outgrow-backend" "og-admin")
 
+# CONSTS
 declare origin="https://github.com"
 declare gitUrl="${origin}/${user_or_company}";
 declare directory="git-backup-${user_or_company}"
 
- if [ ! -d "${directory}" ]; then
-     printf "Creating directory \"${directory}\"\n"
-     mkdir "${directory}"
- fi
+if [ ! -d "${directory}" ]; then
+    printf "Creating directory \"%s\"\n" "$directory"
+    mkdir "${directory}"
+fi
 
-cd "${directory}"
+cd "${directory}" || exit
+echo
 
+# Itterate the repos and clone/pull every repo
 for repo in "${repos[@]}"; do
     if [ -d "${repo}" ]; then
         echo "${repo} exists.Executing \"git pull\"";
-        cd "${repo}"
+        cd "${repo}" || exit
         git add .
         git stash save "randomU3UpSuWXM8zpuTJuMZGytoken"
         git pull
