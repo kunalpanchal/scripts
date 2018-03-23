8a045aaa7734d878329c0e89d352e17d4e7c2e7e
Kay <hello@kunalpanchal.in>
2018-03-23 09:52:34 +0530

    Create keylord.bash

---
+++ keylord.bash
@@ -0,0 +1,9 @@
+# KEYLORD CRACK
+
+# Update the date to the date Keylord was downloaded in format
+# month day hour minute year [mm][dd]HH]MM[yy]  
+
+sudo date 0110100018
+open -a Keylord
+sudo sleep 10
+ntpdate -u time.apple.com
